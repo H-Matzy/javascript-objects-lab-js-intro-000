7c96aeddc8406e4e938d26058c4b0b249825c90c
positive-proc-6135 <haydenmatz97@gmail.com>
2020-09-28 20:17:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function updateObjectWithKeyAndValue(obj, key, value){
   return Object.assign({}, obj, { [key]: value});
 }
 function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
-  return obj[key]=value;
+  obj[key] = value;
 }

a05aa1357bcc2f4549a42c084b6ede570efa50fc
positive-proc-6135 <haydenmatz97@gmail.com>
2020-09-28 19:57:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-var recipes = {};
+var recipes = {object, key, variable};
+
+function updateObjectWithKeyAndValue(o, k, v){
+  return recipes
+  var update = Object.assign({object: o},{key: k},{variable=v})
+  return update
+}
